9ffbf721ad9a8b179741810528f8db4e64b5f140
ZacharyJacobCollins <ZacharyJacobCollins@gmail.com>
2017-02-26 11:35:15 -0500

    do stuff

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-	<div id='left-box'>
-		<h2> Drop Earrings Not Bombs</h2>
-		
+	<div id='left-box' class="hidden-md-down" style="font-size: 20px">
+		<h2><strong> Drop Earrings Not Bombs </strong></h2>
+
 		<p>An approximate 11 million people have fled Syria due to the civil war turning their homes into warzones. Without proper grasp of the spoken language in a host country, jobs, education, and community are out of reach for the refugees.
 		</p>
 
@@ -11,8 +11,19 @@
 			Drop Earrings Not Bombs (DENB) is a project that raises funds for and empowers Syrian refugees living in Istanbul, Turkey, by providing language, recreational and skill based workshops for refugees of all ages.
 		</p>
 	</div>
-	<div class="container">	
-	    <div class="video-background">
+	<div class="container">
+		<div class="hidden-md-up">
+			<h2> Drop Earrings Not Bombs</h2>
+			
+			<p>An approximate 11 million people have fled Syria due to the civil war turning their homes into warzones. Without proper grasp of the spoken language in a host country, jobs, education, and community are out of reach for the refugees.
+			</p>
+
+			<p>
+				Drop Earrings Not Bombs (DENB) is a project that raises funds for and empowers Syrian refugees living in Istanbul, Turkey, by providing language, recreational and skill based workshops for refugees of all ages.
+			</p>
+		</div>
+
+	    <div class="video-background hidden-md-down">
 	      <div class="video-foreground">
 	        <video id="video-bg" class="video-js vjs-16-9 vjs-default-skin" preload="auto" width="1280" height="720" data-setup="{}" muted autoplay loop>
 	            <source src="{{ asset('video/media/videos/video-bg.mp4') }}" type="video/webm">
