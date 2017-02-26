48d30621d83fc1e4c4e9c4eecb47bad111a3bcc2
ZacharyJacobCollins <ZacharyJacobCollins@gmail.com>
2017-02-26 01:44:29 -0500

    fixed top nav bar

--- resources/views/components/top_nav.blade.php
+++ resources/views/components/top_nav.blade.php
@@ -35,7 +35,7 @@
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse row" id="navbarNavAltMarkup">
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav col-12">
       <a class="nav-item nav-link col-1" href="/">Home <span class="sr-only">(current)</span></a>
       <a class="nav-item nav-link col-2" href="/crew">Crew</a>
